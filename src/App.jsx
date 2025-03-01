a1d88403e9221b99b042425bf8277c9a8079b2d1
Lonis <164430029+lonis752@users.noreply.github.com>
2025-02-28 16:34:00 -0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Project1</section>
+      <section>Project2</section>
+      <section>Project3</section>
+      <section>Contact</section>
+    </div>
+  );
+}
+
+export default App;
